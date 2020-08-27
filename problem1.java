5a1695b1ae8dcdf0b9df68dac39f97d561eefbd0
Kyle McNeill <McNeillK97@gmail.com>
2020-08-27 11:30:39 -0700

    Finished problem 1

--- problem1.java
+++ problem1.java
@@ -1,7 +1,9 @@
 
-/*  This my solution to problem 1 in Project Euler
+/*  
 *   Name: Kyle McNeill
-*   Github:
+*   This is my solution to problem 1 in Project Euler
+*   Github: https://github.com/McNeillK97/Project-Euler
+*   Prompt: https://projecteuler.net/problem=1
 */
    class problem1 
    { 
@@ -9,7 +11,16 @@
        // Prints "Hello, World" to the terminal window. 
        public static void main(String args[]) 
        { 
+            int sum = 0;
 
+            // Loops from 0 to 1000 checking if is divisible by 3 or 5, if so, adds to sum
+            for(int x = 0; x < 1000; x++){
+                if(x % 3 == 0 || x % 5 ==0){
+                    sum += x;
+                }
+            }
+
+            System.out.println(sum);
        } 
    } 
    
