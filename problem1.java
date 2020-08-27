3b8d7fbc7b4346661646fd368eb2f06bba138d65
Kyle McNeill <McNeillK97@gmail.com>
2020-08-27 11:22:09 -0700

    Inital commit
    Started problem 1

---
+++ problem1.java
@@ -0,0 +1,15 @@
+
+/*  This my solution to problem 1 in Project Euler
+*   Name: Kyle McNeill
+*   Github:
+*/
+   class problem1 
+   { 
+       // Your program begins with a call to main(). 
+       // Prints "Hello, World" to the terminal window. 
+       public static void main(String args[]) 
+       { 
+
+       } 
+   } 
+   
